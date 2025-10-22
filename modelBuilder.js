9e0be4f79349e2244e9eae6cb402b7cf2f3160b3
LiShuai1025 <shuli@edu.hse.ru>
2025-10-22 17:54:20 +0300

    Update modelBuilder.js

--- modelBuilder.js
+++ modelBuilder.js
@@ -7,6 +7,8 @@ class ModelBuilder {
                 return this.createImprovedDenseModel(inputDim, outputDim);
             case 'deep_dense':
                 return this.createDeepDenseModel(inputDim, outputDim);
+            case 'rnn':
+                return this.createRNNModel(inputDim, outputDim);
             default:
                 return this.createImprovedDenseModel(inputDim, outputDim);
         }
@@ -151,6 +153,43 @@ class ModelBuilder {
 
         model.compile({
             optimizer: tf.train.adam(0.0005),
+            loss: 'categoricalCrossentropy',
+            metrics: ['accuracy']
+        });
+
+        return model;
+    }
+
+    static createRNNModel(inputDim, outputDim) {
+        const model = tf.sequential();
+        
+        // Reshape input for RNN: [batch, timesteps, features]
+        model.add(tf.layers.lstm({
+            inputShape: [8, 1], // 8 timesteps, 1 feature per timestep
+            units: 64,
+            returnSequences: false,
+            kernelInitializer: 'glorotNormal'
+        }));
+        
+        model.add(tf.layers.batchNormalization());
+        model.add(tf.layers.dropout({ rate: 0.3 }));
+        
+        model.add(tf.layers.dense({
+            units: 32,
+            activation: 'relu',
+            kernelInitializer: 'heNormal'
+        }));
+        
+        model.add(tf.layers.dropout({ rate: 0.2 }));
+        
+        model.add(tf.layers.dense({
+            units: outputDim,
+            activation: 'softmax',
+            kernelInitializer: 'glorotNormal'
+        }));
+
+        model.compile({
+            optimizer: tf.train.adam(0.001),
             loss: 'categoricalCrossentropy',
             metrics: ['accuracy']
         });
